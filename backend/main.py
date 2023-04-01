8096f6895cd40fd8254f5e33ab474c261fd0c763
Bong <songning.lan@gmail.com>
2023-03-31 22:30:28 -0400

    cleanup code

--- backend/main.py
+++ backend/main.py
@@ -1,42 +1,30 @@
-import time
 import json
-#import flask
 
-def sleepCount(start:int, end:int):
-    return end - start
-
-
-#[{user:String, timeSlept:Int, timeStopSleep:Int, dayOfTheWeek:Int, weekSum:Int, sleepDate:Int, wakeupDate:Int, monday:Int, tuesday, wednesday, ...}, ...]
 with open('data.json') as file:
     data = json.load(file)
 
-#for i in data:
-#    i["weekSum"] += sleepCount(i["timeSlept"], i["timeStopSleep"]) # 7 times
-
 class user:
     def __init__(self,
-                user,
-                timeSlept,
-                timeStopSleep,
-                dayOfTheWeek,
-                weekSum,
-                sleepDate,
-                wakeupDate,
-                monday,
-                tuesday,
-                wednesday,
-                thursday,
-                friday,
-                saturday,
-                sunday,
-                today):
+                user:str,
+                timeSlept:int,
+                timeStopSleep:int,
+                weekSum:int,
+                sleepDate:int,
+                wakeUpDate:int,
+                monday:int,
+                tuesday:int,
+                wednesday:int,
+                thursday:int,
+                friday:int,
+                saturday:int,
+                sunday:int,
+                today:int):
         self.user = user
         self.timeSlept = timeSlept
         self.timeStopSleep = timeStopSleep
-        self.dayOfTheWeek = dayOfTheWeek
         self.weekSum = weekSum
         self.sleepDate = sleepDate
-        self.wakeupDate = wakeupDate
+        self.wakeUpDate = wakeUpDate
         self.monday = monday
         self.tuesday = tuesday
         self.wednesday = wednesday
@@ -45,10 +33,9 @@ class user:
         self.saturday = saturday
         self.sunday = sunday
         self.today = today
-        #sleepCount = lambda self : self.timeStopSleep - self.timeSlept if self.wakeupDate == self.sleepDate else 24 - self.timeSlept + self.timeStopSleep
 
     def sleepCount(self):
-        return self.timeStopSleep - self.timeSlept if self.wakeupDate == self.sleepDate else 24 - self.timeSlept + self.timeStopSleep
+        return self.timeStopSleep - self.timeSlept if self.wakeUpDate == self.sleepDate else 24 - self.timeSlept + self.timeStopSleep
     
     def amountSleptCompare(self):
         if(self.today == 1):
@@ -67,23 +54,21 @@ class user:
             return self.sunday - self.saturday
     
     def todaySleep(self):
+        self.weekSum += self.sleepCount()
         if(self.today == 1):
-            self.monday = sleepCount()
+            self.monday = self.sleepCount()
         elif(self.today == 2):
-            self.tuesday = sleepCount()
+            self.tuesday = self.sleepCount()
         elif(self.today == 3):
-            self.wednesday = sleepCount()
+            self.wednesday = self.sleepCount()
         elif(self.today == 4):
-            self.thursday = sleepCount()
+            self.thursday = self.sleepCount()
         elif(self.today == 5):
-            self.friday = sleepCount()
+            self.friday = self.sleepCount()
         elif(self.today == 6):
-            self.saturday = sleepCount()
+            self.saturday = self.sleepCount()
         elif(self.today == 7):
-            self.sunday = sleepCount()
+            self.sunday = self.sleepCount()
 
-
-        
-        
-    users = [user(i["user"], i["timeSlept"], i["timeStopSleep"], i["dayofTheWeek"], i["weekSum"], i["sleepDate"], i["wakeuppDate"], i["monday"], i["tuesday"], i["wednesday"], i["thursday"], i["friday"], i["saturday"], i["sunday"], i["today"]) for i in data]
+users = [user(i["user"], i["timeSlept"], i["timeStopSleep"], i["weekSum"], i["sleepDate"], i["wakeUpDate"], i["monday"], i["tuesday"], i["wednesday"], i["thursday"], i["friday"], i["saturday"], i["sunday"], i["today"]) for i in data]
     
