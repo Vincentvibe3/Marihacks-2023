5fed075d76805c666f688f6983019be017a83184
Vincent <vincentvibe3@gmail.com>
2023-04-01 08:40:38 -0400

    changes to frontend

--- frontend/src/routes/view/+page.svelte
+++ frontend/src/routes/view/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
     import type { PageData } from './$types';
     
     export let data: PageData;
+
+    let sleepChart:HTMLCanvasElement
+
     onMount(()=>{
 		new Chart(
 			sleepChart, {
---
+++ frontend/src/stores.js
@@ -0,0 +1,3 @@
+import { writable } from 'svelte/store'
+export const transparent = writable(true)
+export const userToken = writable("")
