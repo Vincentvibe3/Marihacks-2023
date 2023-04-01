dc1d04620ff7487a8ccf0c279b50d76a109dc1d8
Bong <songning.lan@gmail.com>
2023-04-01 11:39:43 -0400

    fak yuo vincent

--- backend/main.py
+++ backend/main.py
@@ -20,78 +20,71 @@ class User:
                 saturday:int=0,
                 sunday:int=0,
                 today:int=0):
-        self.username = username
-        self.timeSlept = timeSlept
-        self.timeStopSleep = timeStopSleep
-        self.sleepDate = sleepDate
-        self.wakeUpDate = wakeUpDate
-        self.monday = monday
-        self.tuesday = tuesday
-        self.wednesday = wednesday
-        self.thursday = thursday
-        self.friday = friday
-        self.saturday = saturday
-        self.sunday = sunday
-        self.today = today
+        self.SleepData = [username, timeSlept, timeStopSleep, weekSum, sleepDate, wakeUpDate, monday, tuesday, wednesday, thursday, friday, saturday, sunday, today]
 
     def sleepCount(self):
-        return self.timeStopSleep - self.timeSlept if self.wakeUpDate == self.sleepDate else 24 - self.timeSlept + self.timeStopSleep
+        return self.SleepData[2] - self.SleepData[1] if self.SleepData[5] == self.SleepData[4] else 24 - self.SleepData[1] + self.SleepData[2]
     
     def amountSleptCompare(self): 
-        if(self.today == 1):
-            return self.monday - self.sunday
-        elif(self.today == 2):
-            return self.tuesday - self.monday
-        elif(self.today == 3):
-            return self.wednesday - self.tuesday
-        elif(self.today == 4):
-            return self.thursday - self.wednesday
-        elif(self.today == 5):
-            return self.friday - self.thursday
-        elif(self.today == 6):
-            return self.saturday - self.friday
-        elif(self.today == 7):
-            return self.sunday - self.saturday
+        if(self.SleepData[13] == 1):
+            return self.SleepData[6] - self.SleepData[12]
+        elif(self.SleepData[13] == 2):
+            return self.SleepData[7] - self.SleepData[6]
+        elif(self.SleepData[13] == 3):
+            return self.SleepData[8] - self.SleepData[7]
+        elif(self.SleepData[13] == 4):
+            return self.SleepData[9]- self.SleepData[8]
+        elif(self.SleepData[13] == 5):
+            return self.SleepData[10] - self.SleepData[9]
+        elif(self.SleepData[13] == 6):
+            return self.SleepData[11] - self.SleepData[10]
+        elif(self.SleepData[13] == 7):
+            return self.SleepData[12] - self.SleepData[11]
     
     def todaySleep(self): #call once a day
-        self.weekSum += self.sleepCount()
-        if(self.today == 1):
-            self.monday = self.sleepCount()
-        elif(self.today == 2):
-            self.tuesday = self.sleepCount()
-        elif(self.today == 3):
-            self.wednesday = self.sleepCount()
-        elif(self.today == 4):
-            self.thursday = self.sleepCount()
-        elif(self.today == 5):
-            self.friday = self.sleepCount()
-        elif(self.today == 6):
-            self.saturday = self.sleepCount()
-        elif(self.today == 7):
-            self.sunday = self.sleepCount()
+        self.SleepData[3] += self.sleepCount()
+        if(self.SleepData[13] == 1):
+            self.SleepData[6] = self.sleepCount()
+        elif(self.SleepData[13] == 2):
+            self.SleepData[7] = self.sleepCount()
+        elif(self.SleepData[13] == 3):
+            self.SleepData[8] = self.sleepCount()
+        elif(self.SleepData[13] == 4):
+            self.SleepData[9] = self.sleepCount()
+        elif(self.SleepData[13] == 5):
+            self.SleepData[10] = self.sleepCount()
+        elif(self.SleepData[13] == 6):
+            self.SleepData[11] = self.sleepCount()
+        elif(self.SleepData[13] == 7):
+            self.SleepData[12] = self.sleepCount()
     
     def reset(self): #call once a week
-        self.monday = 0
-        self.tuesday = 0
-        self.wednesday = 0
-        self.thursday = 0
-        self.friday = 0
-        self.saturday = 0
+        self.SleepData[6] = 0
+        self.SleepData[7] = 0
+        self.SleepData[8] = 0
+        self.SleepData[9] = 0
+        self.SleepData[10] = 0
+        self.SleepData[11] = 0
         #do not reset sunday
-        self.weekSum = 0
-        self.timeSlept = 0
-        self.sleepDate = 0
-        self.wakeUpDate = 0
-        self.timeStopSleep = 0
+        self.SleepData[3] = 0
+        self.SleepData[1] = 0
+        self.SleepData[4] = 0
+        self.SleepData[5] = 0
+        self.SleepData[2] = 0
         #do not reset today
+
+    def __str__(self):
+        return str(self.SleepData)
 
 users = [User(i["username"], i["sleepData"]["timeSlept"], i["sleepData"]["timeStopSleep"], i["sleepData"]["weekSum"], i["sleepData"]["sleepDate"], i["sleepData"]["wakeUpDate"], i["sleepData"]["monday"], i["sleepData"]["tuesday"], i["sleepData"]["wednesday"], i["sleepData"]["thursday"], i["sleepData"]["friday"], i["sleepData"]["saturday"], i["sleepData"]["sunday"], i["sleepData"]["today"]) for i in data]
 
 def leaderboard():
     return users.sort(key=lambda x: x.weekSum, reverse=True)
 
-with open('D:/SunWeiwei/Repo/Marihacks-2023/backend/quotes.txt', 'r', encoding='utf-8') as f:
+#with open('D:/SunWeiwei/Respo/Marihacks-2023/backend/quotes.txt', 'r', encoding='utf-8') as f:
     quotes = [line.strip() for line in f]
 
-def getMessage(): #just call this function once a day
+#def getMessage(): #just call this function once a day
     return random.choice(quotes)
+
+print(users[1])
