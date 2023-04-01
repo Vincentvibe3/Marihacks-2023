ad9cf905347200c9f2793bc50513f26864f61e13
Vincent <vincentvibe3@gmail.com>
2023-03-31 20:03:11 -0400

    restructure stuff

---
+++ frontend/src/routes/view/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+    onMount(()=>{
+		new Chart(
+			sleepChart, {
+				type:'bar',
+				data: {
+					labels: ['Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday', 'Satruday', 'Sunday'],
+					datasets: [{
+						label: 'Hours of sleep',
+						data: [12, 19, 3, 5, 2, 3],
+						borderWidth: 1
+					}]
+					},
+					options: {
+					scales: {
+						y: {
+						beginAtZero: true
+						}
+					}
+				}
+			}
+		)
+	})
+</script>
+<canvas bind:this={sleepChart} id="sleepChart"></canvas>
+<svelte:head>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</svelte:head>
